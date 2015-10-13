665fbf45ccbbe59e75a78d947ff032865ee87315
ApurvaSinha17 <apurva.sinha.17@outlook.com>
2015-10-13 17:31:33 -0400

    Updated BinaryTreeCharNode and ExpressionTree java classes

--- BinaryTree/src/createBinaryTree/BinaryTreeCharNode.java
+++ BinaryTree/src/createBinaryTree/BinaryTreeCharNode.java
@@ -1,25 +1,51 @@
 package createBinaryTree;
 
-public class BinaryTreeCharNode {
+/**
+ * @author Apurva Kumar Sinha
+ * Binary tree node having a char value
+ *
+ */
+public final class BinaryTreeCharNode {
 
-	private char data;
+	private final char data;
 	private BinaryTreeCharNode leftNode;
 	private BinaryTreeCharNode rightNode;
+
+	/**
+	 * @param char data
+	 */
+	public BinaryTreeCharNode(char data){
+		this.data = data;
+	}
+	
+	/**
+	 * @return char data
+	 */
 	public char getData() {
 		return data;
 	}
-	public void setData(char data) {
-		this.data = data;
-	}
+	
+	/**
+	 * @return leftNode
+	 */
 	public BinaryTreeCharNode getLeftNode() {
 		return leftNode;
 	}
+	/**
+	 * @param leftNode
+	 */
 	public void setLeftNode(BinaryTreeCharNode leftNode) {
 		this.leftNode = leftNode;
 	}
+	/**
+	 * @return rightNode
+	 */
 	public BinaryTreeCharNode getRightNode() {
 		return rightNode;
 	}
+	/**
+	 * @param rightNode
+	 */
 	public void setRightNode(BinaryTreeCharNode rightNode) {
 		this.rightNode = rightNode;
 	}
--- BinaryTree/src/problems/ExpressionTree.java
+++ BinaryTree/src/problems/ExpressionTree.java
@@ -4,48 +4,73 @@ import java.util.Stack;
 
 import createBinaryTree.BinaryTreeCharNode;
 
+/**
+ * @author Apurva Kumar Sinha
+ * Java program to build an expression tree from a post-fix expression
+ * Input: ABC*+D/ (post-fix expression)
+ * Output: A+B*C/D (in-fix expression)
+ * Presently this code supports just the following binary operators: + * - / %
+ */
 public class ExpressionTree {
 
 	public static void main(String[] args) {
 		
-		//Valid Postfix expression
-		String postFixExpr = "ABC*+D/";
-		char[] postFixArr = postFixExpr.toCharArray();
-				
-		BinaryTreeCharNode rootNode = buildExprTree(postFixArr);
-		
-		//Inorder traversal of binary tree
-		inOrder(rootNode);
+		if(args.length == 0 || "".equals(args[0])){
+			System.out.println("No input was provided");
+		} else {
+			String postFixExpr = args[0];
+			char[] postFixArr = postFixExpr.toCharArray();			
+			BinaryTreeCharNode rootNode;
+			try {
+				rootNode = buildExprTree(postFixArr);
+				displayAsInfixExpr(rootNode);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}			
+			
+		}		
 	}
 
-	private static void inOrder(BinaryTreeCharNode node){
+	/**
+	 * @param node (Root node of the Binary tree)
+	 * Prints out the in-fix expression by doing an in-order traversal of the binary tree
+	 */
+	private static void displayAsInfixExpr(BinaryTreeCharNode node){
 		
-		if(node == null)
-			return;
+		if(node != null){
+			displayAsInfixExpr(node.getLeftNode());
+			System.out.print(node.getData());
+			displayAsInfixExpr(node.getRightNode());
+		}
 		
-		inOrder(node.getLeftNode());
-		System.out.print(node.getData()+" ");
-		inOrder(node.getRightNode());;
 	}
 	
-	private static BinaryTreeCharNode buildExprTree(char[] post){
+	/**
+	 * @param post (Post-fix expression as a char array)
+	 * @return Root node of the constructed binary tree
+	 */
+	private static BinaryTreeCharNode buildExprTree(char[] post) throws Exception{
 		
 		Stack<BinaryTreeCharNode> stack = new Stack<BinaryTreeCharNode>();
 		
 		for(int i=0;i<post.length;i++){
 			
 			if(post[i] == '+' || post[i] == '*' || post[i] == '-' || post[i] == '/' || post[i] == '%'){
+				if(stack.isEmpty()){
+					throw new RuntimeException("Invalid input");
+				}
 				BinaryTreeCharNode rNode = stack.pop();
+				if(stack.isEmpty()){
+					throw new RuntimeException("Invalid input");
+				}
 				BinaryTreeCharNode lNode = stack.pop();
-				BinaryTreeCharNode node = new BinaryTreeCharNode();
-				node.setData(post[i]);
+				BinaryTreeCharNode node = new BinaryTreeCharNode(post[i]);
 				node.setLeftNode(lNode);
 				node.setRightNode(rNode);
 				stack.push(node);
 			}
 			else{
-				BinaryTreeCharNode node = new BinaryTreeCharNode();
-				node.setData(post[i]);
+				BinaryTreeCharNode node = new BinaryTreeCharNode(post[i]);
 				node.setLeftNode(null);
 				node.setRightNode(null);
 				stack.push(node);
